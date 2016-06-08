1d39532d19bc21742c1f5eb89df845716d59a226
Eduardo Gonzalez <eduardo.gch@gmail.com>
2016-06-08 12:09:02 -0400

    Follow conventions names for gulp plugins

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+var path = require('path');
+var spawn = require('child_process').spawn;
+var chimpPath = path.resolve(process.cwd() + '/node_modules/.bin/chimp');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
 
-function helloWorld() {
-    console.log("hello World");
+// Consts
+const PLUGIN_NAME = 'gulp-chimp';
+
+function gulpChimp() {
+    var args = [];
+    var chimp = spawn(chimpPath, args);
+
+    chimp.stdout.on('data', function(data) {
+      process.stdout.write(data.toString());
+    });
+
+    chimp.stderr.pipe(process.stdout);
 }
 
+gulpChimp();
+
 var index = {
-    helloWorld: helloWorld
+    gulpChimp: gulpChimp
 };
 
 module.exports = index;
